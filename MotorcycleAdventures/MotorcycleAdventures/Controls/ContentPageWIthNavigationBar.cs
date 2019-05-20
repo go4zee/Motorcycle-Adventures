6d24f4183af5b943a478e8e13bd019c587b38717
Syed Abbas Zakir Hussain <zhsyedabbas@gmail.com>
2019-05-19 22:19:53 -0400

    Init

---
+++ MotorcycleAdventures/MotorcycleAdventures/Controls/ContentPageWIthNavigationBar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace MotorcycleAdventures.Controls
+{
+    public class ContentPageWithNavigationBar : ContentPage
+    {
+        public ContentPageWithNavigationBar()
+        {
+            NavigationPage.SetTitleView(this, new HeaderView());
+        }
+    }
+}
---
+++ MotorcycleAdventures/MotorcycleAdventures/Core/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MotorcycleAdventures.Core
+{
+    public class Constants
+    {
+        public static Color Orange = Color.FromRgb(255, 167, 10);
+
+        public static Color SeperatorColor = Color.LightGray;
+
+        public static Color EntryTextColor = Color.FromHex("737373");
+
+        public static Color BottonIconDefault = Color.FromHex("f0f0f0");
+
+
+        public static class Unit
+        {
+            public static string In = "in";
+            public static string mm = "mm";
+        }
+
+    }
+}
