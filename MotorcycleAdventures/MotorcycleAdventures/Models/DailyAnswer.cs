29262886890780609fc2ea1bcd20a6843a33e555
Syed Abbas Zakir Hussain <zhsyedabbas@gmail.com>
2019-05-19 22:26:11 -0400

    Add DailyAnswer

---
+++ MotorcycleAdventures/MotorcycleAdventures/Models/DailyAnswer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MotorcycleAdventures.Models
+{
+    public class DailyAnswer
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Answer { get; set; }
+
+        public DateTime AnsweredOn { get; set; }
+
+        public DailyAnswer()
+        {
+            AnsweredOn = DateTime.Now;
+        }
+    }
+}
